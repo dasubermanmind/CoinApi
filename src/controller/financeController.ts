d3d7625e981d3c30389ff0916a8f23bbf14e6fc0
EOstling <it.treugott@gmail.com>
2021-03-06 11:38:52 -0500

    wip: get functionality

--- src/controller/financeController.ts
+++ src/controller/financeController.ts
@@ -1,10 +1,39 @@
+import {getRepository, Repository} from "typeorm";
+import { Request, Response } from "express";
+import Crypto from "../entities/crypto";
 
 
-class FinanceController{
-    get = ()=>{
-        console.log('financeController works')
-        return{data: 'works'};
-    };
+class FinanceController {
+    private repository: Repository<Crypto>;
+
+    constructor( ){
+        this.repository = getRepository(Crypto);
+    }
+
+    /*
+    * There are 5 different QueryBuilder types available:
+    * SelectQueryBuilder
+    * InsertQueryBuilder
+    * UpdateQueryBuilder
+    * DeleteQueryBuilder
+    * RelationQueryBuilder
+    * */
+
+    // Define functionality of the API
+    getAll = async (request: Request, response: Response): Promise<Response> =>{
+        let cryptoQuery = request.query;
+        let query = this.repository.createQueryBuilder("crypto");
+
+        const result = await query.getMany();
+        return response.send(result);
+    }
+
+    get = async (request: Request, response: Response): Promise<Response>=>{
+        const { id } = request.params;
+        // validate it
+        // query it
+        // return it
+    }
 }
 
 export = new FinanceController();
