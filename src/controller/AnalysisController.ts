b093d75fe71e08a24ac1c5eb95e163333e75d306
EOstling <it.treugott@gmail.com>
2021-03-06 13:15:19 -0500

    future functionality

---
+++ src/controller/AnalysisController.ts
@@ -0,0 +1,13 @@
+import {getRepository, Repository} from "typeorm";
+import { Request, Response } from "express";
+
+
+class AnalysisController {
+
+    // Get the most recent analysis
+    // Update most recent analysis
+    // create a new analysis
+}
+
+export = new AnalysisController();
+
