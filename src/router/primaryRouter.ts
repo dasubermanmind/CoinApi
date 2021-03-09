114a025c478383dc2468ede1c9d51782659c511c
EOstling <it.treugott@gmail.com>
2021-03-09 09:01:07 -0500

    Added swagger endpoint

--- src/router/primaryRouter.ts
+++ src/router/primaryRouter.ts
@@ -1,11 +1,17 @@
 import express = require("express")
 import FinanceController from "../controller/financeController";
+import { swaggerDocument } from "../Swagger/swaggerDocument";
+const swaggerUi = require('swagger-ui-express');
 
+// TODO: Make a Swagger Router
 function primaryRouter(): express.Router{
     const router = express.Router();
     const controller  = new FinanceController();
     router.route("/fin").get(controller.getAll);
     router.route("/fin/:id").get(controller.get);
+    router.use('/api-docs', swaggerUi.serve);
+    router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
     return router;
 }
 
