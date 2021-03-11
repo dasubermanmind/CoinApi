a71cc3bfdbbaa121ce0b65e4bd96ef59bafc7035
EOstling <it.treugott@gmail.com>
2021-03-11 07:35:51 -0500

    Added Analysis Repository

---
+++ src/Repository/AnalysisRepository.ts
@@ -0,0 +1,17 @@
+import { getRepository } from "typeorm";
+import {Analysis} from "@/entities/analysis";
+
+export const getAllAnalysis = async (): Promise<Analysis[]> => {
+    const analysisRepository = getRepository(Analysis);
+    return analysisRepository.find();
+};
+
+export const  getAnalysis = async (id: number): Promise<Analysis | undefined> => {
+    const analysisRepository = getRepository(Analysis);
+
+    const analysis = await analysisRepository.findOne({ analysis_id: id });
+    if (!analysis) return undefined;
+
+    return analysis;
+};
+
