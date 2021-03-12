c0a98ba677a839fc6acd8618297c1f5b50f8dd52
EOstling <it.treugott@gmail.com>
2021-03-12 13:19:05 -0500

    Boilerplate tests

---
+++ __tests__/Unit/Controller/userController.test.ts
@@ -0,0 +1,28 @@
+import typeorm = require('typeorm');
+
+jest.mock('typeorm',
+    () => ({
+        getRepository: jest.fn(),
+    }));
+
+describe("Controller", () => {
+    // Arrange all global objects
+    // controller
+    let mockReq;
+    let mockRes;
+
+    beforeEach(async () => {});
+
+    describe('User', () => {
+        test('getAll', async () => {
+            // Arrange
+            typeorm.getRepository = jest.fn().mockReturnValue({
+                createQueryBuilder: jest.fn().mockResolvedValue('mockQuery')
+            });
+
+            // controller
+            // call the function with mock Req & mock res
+            // assert
+        });
+    });
+});
